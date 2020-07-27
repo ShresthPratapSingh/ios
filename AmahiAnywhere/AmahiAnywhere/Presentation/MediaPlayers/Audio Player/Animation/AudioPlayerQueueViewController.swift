e8f5b783b003251cb4686250e8156f5ddf051c5e
Shresth Pratap Singh <shresth@MacBook-Air.local>
2020-07-27 20:30:32 +0530

    pulled changes

--- AmahiAnywhere/AmahiAnywhere/Presentation/MediaPlayers/Audio Player/Animation/AudioPlayerQueueViewController.swift
+++ AmahiAnywhere/AmahiAnywhere/Presentation/MediaPlayers/Audio Player/Animation/AudioPlayerQueueViewController.swift
@@ -27,25 +27,7 @@ class AudioPlayerQueueViewController:UIViewController{
     }()
     
     //MARK:- VC Lifecycle
-    
-    init() {
-        super.init(nibName: nil, bundle: nil)
-        setupViews()
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    private func setupViews() {
-        view.addSubview(tableView)
-        tableView.translatesAutoresizingMaskIntoConstraints = false
-        tableView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0).isActive = true
-        tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0).isActive = true
-        tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0).isActive = true
-        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
-    }
-    
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         
